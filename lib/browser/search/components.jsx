f3c6bac8acdbb0da2a804f108ab371dcc09d4ecb
Jim <zicowarn@hotmail.com>
2022-11-03 10:30:28 +0100

    更新
    尝试完成中文适配

--- lib/browser/search/components.jsx
+++ lib/browser/search/components.jsx
@@ -1,33 +1,72 @@
 const React = require('react');
-const {SHOW_SEARCH_RESULTS, HIDE_SEARCH_RESULTS} = require('./actions');
-const {dispatch} = require('../utils');
+const { SHOW_SEARCH_RESULTS, HIDE_SEARCH_RESULTS } = require('./actions');
+const { dispatch } = require('../utils');
 
 class SearchForm extends React.Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
   }
 
-  handleKeyUp (e) {
-    const query = (e.target.value || '').trim();
+  handleKeyUp(e) {
 
+    const query = (e.target.value || '').trim();
+    //console.log('query:' + query);
     if (!query) {
       dispatch(HIDE_SEARCH_RESULTS);
       return;
     }
 
-    if (query.length < 3) { return; }
-
     const results = this.props.search(query);
+    if (results.length > 0) {
+      dispatch(SHOW_SEARCH_RESULTS, { results, query });
+    }
+    else {
+      fetch('lunr.json', { credentials: 'include' })
+        .then(function(res) {
+          return res.json();
+        })
+        .then(function(json) {
+          //console.log('**store**');
+          //console.log(json.store);
+          for (var key in json.store) {
+            if (json.store.hasOwnProperty(key)) { //console.log(key + " -> " + json.store[key]); //body path title
+              //console.log(json.store[key].body.includes(query));
+              if (json.store[key].body.includes(query)) {
+                var displayContent = {
+                  body: generateBody(json.store[key].body, query),
+                  path: json.store[key].path,
+                  title: json.store[key].title
+                };
+                results.push(displayContent);
+              }
+              else if (json.store[key].title.includes(query)) {
+                var displayContent = {
+                  body: generateBody(json.store[key].body, query),
+                  path: json.store[key].path,
+                  title: json.store[key].title
+                };
+                results.push(displayContent);
+              }
+            }
+          }
+          dispatch(SHOW_SEARCH_RESULTS, { results, query });
 
-    dispatch(SHOW_SEARCH_RESULTS, {results, query});
+        });
+
+    }
+
+
 
     if (typeof this.props.onSearch === 'function') {
       this.props.onSearch();
     }
+
+
+
   }
 
-  render () {
+  render() {
 
     if (!this.props.search) { return null; }
 
@@ -38,7 +77,7 @@ class SearchForm extends React.Component {
           placeholder="Search..."
           onKeyUp={this.handleKeyUp.bind(this)}
           autoFocus={this.props.autoFocus} />
-        <button className="dc-btn dc-search-form__btn doc-search-form__btn" aria-label="Search">
+        <button className="dc-btn dc-search-form__btn doc-search-form__btn" aria-label="搜索 ">
           <i className="dc-icon dc-icon--search"></i>
         </button>
       </div>
@@ -46,19 +85,48 @@ class SearchForm extends React.Component {
   }
 }
 
-function SearchResultsTitle ({results, query}) {
+
+function generateBody(body, query) {
+  var indexNumber = body.indexOf(query);
+  var bodyLength = body.length;
+  var returnBody = '';
+  var frontCut = 10;
+  var bodyCut = 100;
+
+  if (indexNumber < frontCut && bodyLength > bodyCut) {
+    returnBody = body.substring(0, bodyCut) + '。。。';
+  }
+  else if (indexNumber < frontCut && bodyLength < bodyCut) {
+    returnBody = body.substring(0, bodyLength);
+  }
+  else if (indexNumber > frontCut && bodyLength > (bodyCut + indexNumber)) {
+    returnBody = '。。。' + body.substring(indexNumber - frontCut, bodyCut + indexNumber) + '。。。';
+  }
+  else if (indexNumber > frontCut && bodyLength < (bodyCut + indexNumber)) {
+    returnBody = '。。。' + body.substring(indexNumber - frontCut, bodyLength);
+  }
+  //add highlight 
+
+  returnBody = returnBody.replace(new RegExp(query, 'g'), '<span style="font-weight: bold;">' + query + '</span>');
+  //<span class="doc-highlight">contact</span>
+  return returnBody;
+}
+
+
+
+function SearchResultsTitle({ results, query }) {
   return (
     <div>
       <h1 className="doc-search-results__title">
-        { results.length ? results.length : 'No' } results for <span className="doc-search-results__title__query">"{query}"</span>
+        搜索 <span className="doc-search-results__title__query">"{query}"</span>，{ results.length ? results.length+'个' : '没有' } 结果 ：
       </h1>
 
-      { !results.length ? <p>There are no results for "{query}". Why not <strong>try typing another keyword?</strong></p> : null }
+      { !results.length ? <p>系统未能搜索 "{query}"到结果.  <strong>试试其他关键字？</strong></p> : null }
     </div>
   );
 }
 
-function SearchResultsList ({results}) {
+function SearchResultsList({ results }) {
   if (!results.length) {
     return null;
   }
@@ -78,8 +146,6 @@ function SearchResultsList ({results}) {
               onClick={handleSearchResultLinkClick}>
               {result.title}
             </a>
-            <span className="doc-search-results__list__score-divider">|</span>
-            <span className="doc-search-results__list__score">score: {result.score.toFixed(2)}</span>
             <p dangerouslySetInnerHTML={createMarkup(result.body)}></p>
           </li>
         );
@@ -88,4 +154,4 @@ function SearchResultsList ({results}) {
   );
 }
 
-module.exports = {SearchForm, SearchResultsTitle, SearchResultsList};
+module.exports = { SearchForm, SearchResultsTitle, SearchResultsList };
